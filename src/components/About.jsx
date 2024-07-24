d7f08fd6768e31e100c3f04677da2536323d69d1
Kaozhi Chen <kylerchen16@gmail.com>
2024-07-24 13:01:16 -0400

    finish About page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { FaArrowRight } from 'react-icons/fa';
-import fish from '../assets/Food Images/fish3.png';
+import steak from '../assets/Food Images/steak.png';
+// import fish from '../assets/Food Images/fish3.png';
 const About = () => {
   return (
     <div className='section' id='about'>
-      <div className='grid md:grid-cols-2 items-center mb-10'>
+      {/* The best Chinese Food */}
+      <div className='grid md:grid-cols-2 items-center gap-10 mb-10'>
         <div
           className='flex flex-col justify-center
         items-start gap-6'
@@ -26,7 +28,24 @@ const About = () => {
           </div>
         </div>
         <div className='md:row-start-1'>
-          <img src={fish} alt='fish' />
+          <img src={steak} alt='fish' />
+        </div>
+      </div>
+      {/* cheif */}
+      <div className='grid md:grid-cols-2 items-center gap-10'>
+        <div>
+          <div className='text-xl sm:text-3xl font-bold mb-6'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </div>
+          <p className='text-sm opacity-80'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae
+            debitis nihil soluta totam animi! Enim explicabo recusandae, eaque,
+            eius eligendi distinctio mollitia debitis eveniet commodi dolores
+            quam, laudantium ipsam velit.
+          </p>
+        </div>
+        <div>
+          <img src={steak} alt='steak' />
         </div>
       </div>
     </div>
